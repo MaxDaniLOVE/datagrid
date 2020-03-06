6162d10f535da3bebf7c8c412621bd8f1426d5b1
MaxDaniLOVE <mahseeen@gmail.com>
2020-03-06 10:06:38 +0300

    fix: fix sort

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -8,20 +8,6 @@ const initialState = {
   isSorted: false
 }
 
-const deleteRow = (state, id) => {
-  const { data } = state;
-  const deletedIdx = data.findIndex((el) => el.id === id);
-  data.splice(deletedIdx, 1)
-    const newData = [
-      ...data.slice(0, deletedIdx),
-      ...data.slice(deletedIdx)
-    ]
-  return {
-    ...state,
-    data: newData
-  }
-}
-
 const filterByInput = (state, value) => {
   const filteredData = state.data.filter(({country, name, job}) => {
     return country.toLowerCase().includes(value)
@@ -34,13 +20,19 @@ const filterByInput = (state, value) => {
   return {
     ...state,
     filteredData,
-    isFiltered: true
+    isFiltered: true,
+    filter: value
   }
 }
 
 const sortFunction = (state, activeSort) => {
   const { key, index } = activeSort;
-  if (state.activeSort.key === key && state.activeSort.index === index) return {...state, isSorted: false}
+  if (state.activeSort.key === key
+        && state.activeSort.index === index
+        && !Object.keys(activeSort).length
+        && state.activeSort.index + index !== 0
+        && state.activeSort.index
+      ) return {...state, isSorted: false, activeSort: {}}
   const helpingArray = [...state.data];
   const sortedData = helpingArray.sort((a, b) => {
     if (a[key] > b[key]) return 1 * index;
@@ -52,6 +44,26 @@ const sortFunction = (state, activeSort) => {
     sortedData,
     activeSort,
     isSorted: true
+  }
+}
+
+const deleteRow = (state, id) => {
+  const { data } = state;
+  const deletedIdx = data.findIndex((el) => el.id === id);
+  data.splice(deletedIdx, 1)
+    const newData = [
+      ...data.slice(0, deletedIdx),
+      ...data.slice(deletedIdx)
+    ]
+  if (state.isFiltered) {
+    return filterByInput(state, state.filter)
+  }
+  if (state.isSorted) {
+    return sortFunction(state, state.activeSort)
+  }
+  return {
+    ...state,
+    data: newData,
   }
 }
 
