d6818f0701b47a557474c8c9eeb9383cecc75cd9
MaxDaniLOVE <mahseeen@gmail.com>
2020-03-11 10:29:23 +0300

    feat: add loading scv document

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -27,13 +27,22 @@ class App extends Component {
                     isFiltered ? filteredData :
                     isSorted ? sortedData : [];
     const displayedData = allData.slice((activePage - 1) * 100, activePage * 100);
+    const csvData = "data:text/csv;charset=utf-8,"
+    + 'id, name, country, job, last salary, date of application, expirience in JS\n'
+    + displayedData.map(e => Object.values(e).join(",")).join("\n");
+    const encodedUri = encodeURI(csvData);
     const defaultValue = localStorage.filter === 'true' || localStorage.filter === 'false' ? '' : localStorage.filter;
     return (
       <div className="container">
         <h1>Data grid</h1>
         <div className="form-group">
           <h5>Search:</h5>
-          <input defaultValue={defaultValue} type="text" className="form-control" placeholder="Type name, country or job" onChange={(e) => onInputChange(e)} />
+          <div className="input-wrapper">
+            <input defaultValue={defaultValue} type="text" className="form-control" placeholder="Type name, country or job" onChange={(e) => onInputChange(e)} />
+            <a className="btn btn-success load-btn" href={encodedUri} download="data.csv">
+              <i className="fa fa-save" />
+            </a>
+          </div>
         </div>
         <Table data={displayedData} activeSort={activeSort} setActiveSort={setActiveSortFunc} />
         <PageSwitcher pages={Math.ceil(allData.length / 100)} />
